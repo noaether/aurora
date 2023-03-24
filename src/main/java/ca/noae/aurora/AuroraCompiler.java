c256f4e360afe2bfffcb970900299e8f2268033e
noadev <67566122+thenoadev@users.noreply.github.com>
2023-03-24 00:16:27 -0400

    more boilerplate woohoo

--- src/main/java/ca/noae/aurora/AuroraCompiler.java
+++ src/main/java/ca/noae/aurora/AuroraCompiler.java
@@ -1,4 +1,32 @@
 package ca.noae.aurora;
 
+import ca.noae.aurora.codegen.Instruction;
+import ca.noae.aurora.lexer.Token;
+import ca.noae.aurora.parser.SyntaxTree;
+
+import java.text.ParseException;
+import java.util.List;
+
 public class AuroraCompiler {
+    private final List<Token> tokens;
+    private int currentPosition;
+
+    public AuroraCompiler(List<Token> tokens) {
+        this.tokens = tokens;
+        this.currentPosition = 0;
+    }
+
+    public SyntaxTree parse() throws ParseException {
+        // TODO: Implement parsing logic
+        return null;
+    }
+
+    public void analyzeSemantics() /* throws SemanticException */ {
+        // TODO: Implement semantic analysis logic
+    }
+
+    public List<Instruction> generateCode() /* throws CodeGenerationException */ {
+        // TODO: Implement code generation logic
+        return null;
+    }
 }
--- src/main/java/ca/noae/aurora/Main.java
+++ src/main/java/ca/noae/aurora/Main.java
@@ -2,6 +2,10 @@ package ca.noae.aurora;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // TODO: Implement command-line argument handling
+        // TODO: Read input file and create a new scanner object
+        // TODO: Parse input tokens and create an abstract syntax tree
+        // TODO: Perform semantic analysis and generate code
+        // TODO: Write generated code to output file
     }
 }
