7db08a482fe30a2344edb880b9a17c88e281722d
noadev <67566122+thenoadev@users.noreply.github.com>
2023-03-24 00:17:12 -0400

    lexer should be relatively okay, haven't tested it though

--- src/main/java/ca/noae/aurora/lexer/Lexer.java
+++ src/main/java/ca/noae/aurora/lexer/Lexer.java
@@ -1,4 +1,120 @@
 package ca.noae.aurora.lexer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lexer {
+    private final String input;
+    private int currentPosition;
+
+    public Lexer(String input) {
+        this.input = input;
+        this.currentPosition = 0;
+    }
+
+    public List<Token> tokenize() throws LexicalException {
+        List<Token> tokens = new ArrayList<>();
+
+        while (hasNextChar()) {
+            char c = getCurrentChar();
+            if (Character.isDigit(c)) {
+                tokens.add(tokenizeNumber());
+            } else if (Character.isLetter(c)) {
+                tokens.add(tokenizeIdentifierOrKeyword());
+            } else if (c == '"') {
+                tokens.add(tokenizeString());
+            } else {
+                TokenType tokenType = TokenType.getSymbolTokenType(c);
+                if (tokenType != null) {
+                    tokens.add(new Token(tokenType, String.valueOf(c)));
+                } else {
+                    throw new LexicalException("Invalid character: " + c);
+                }
+            }
+            consumeChar();
+        }
+
+        tokens.add(new Token(TokenType.EOF, ""));
+        return tokens;
+    }
+
+    private Token tokenizeNumber() {
+        int startPosition = currentPosition;
+        while (hasNextChar() && Character.isDigit(getCurrentChar())) {
+            consumeChar();
+        }
+        if (hasNextChar() && getCurrentChar() == '.') {
+            consumeChar();
+            while (hasNextChar() && Character.isDigit(getCurrentChar())) {
+                consumeChar();
+            }
+            return new Token(TokenType.DOUBLE_LITERAL, input.substring(startPosition, currentPosition));
+        } else {
+            return new Token(TokenType.INTEGER_LITERAL, input.substring(startPosition, currentPosition));
+        }
+    }
+
+    private Token tokenizeIdentifierOrKeyword() {
+        int startPosition = currentPosition;
+        while (hasNextChar() && (Character.isLetterOrDigit(getCurrentChar()) || getCurrentChar() == '_')) {
+            consumeChar();
+        }
+        String identifier = input.substring(startPosition, currentPosition);
+        TokenType tokenType = TokenType.getKeywordTokenType(identifier);
+        if (tokenType != null) {
+            return new Token(tokenType, "");
+        } else {
+            return new Token(TokenType.IDENTIFIER, identifier);
+        }
+    }
+
+    private Token tokenizeString() throws LexicalException {
+        StringBuilder sb = new StringBuilder();
+        consumeChar(); // Consume the starting double quote
+        while (hasNextChar()) {
+            char c = getCurrentChar();
+            if (c == '"') {
+                consumeChar(); // Consume the ending double quote
+                return new Token(TokenType.STRING_LITERAL, sb.toString());
+            } else if (c == '\\') {
+                consumeChar();
+                if (!hasNextChar()) {
+                    throw new LexicalException("Invalid escape sequence: \\");
+                }
+                char escapeChar = getCurrentChar();
+                switch (escapeChar) {
+                    case 'n' -> sb.append('\n');
+                    case 't' -> sb.append('\t');
+                    case 'r' -> sb.append('\r');
+                    case '\'' -> sb.append('\'');
+                    case '\"' -> sb.append('\"');
+                    case '\\' -> sb.append('\\');
+                    default -> throw new LexicalException("Invalid escape sequence: \\" + escapeChar);
+                }
+            } else {
+                sb.append(c);
+            }
+            consumeChar();
+        }
+        throw new LexicalException("Unterminated string literal");
+    }
+
+    private char getCurrentChar() {
+        return input.charAt(currentPosition);
+    }
+
+    private boolean hasNextChar() {
+        return currentPosition < input.length();
+    }
+
+    private void consumeChar() {
+        currentPosition++;
+    }
+
+    public static class LexicalException extends Exception {
+        public LexicalException(String message) {
+            super(message);
+        }
+    }
+
 }
---
+++ src/main/java/ca/noae/aurora/lexer/TokenType.java
@@ -0,0 +1,124 @@
+package ca.noae.aurora.lexer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum TokenType {
+    // Keywords
+    FUNCTION,
+    RETURN,
+    IF,
+    ELSE,
+    WHILE,
+    FOR,
+    VAR,
+    TRUE,
+    FALSE,
+
+    // Symbols
+    LPAREN("("),
+    RPAREN(")"),
+    LBRACE("{"),
+    RBRACE("}"),
+    LBRACKET("["),
+    RBRACKET("]"),
+    SEMICOLON(";"),
+    COMMA(","),
+    DOT("."),
+    PLUS("+"),
+    MINUS("-"),
+    ASTERISK("*"),
+    SLASH("/"),
+    PERCENT("%"),
+    CARET("^"),
+    BANG("!"),
+    EQUAL("="),
+    LESS("<"),
+    GREATER(">"),
+    AMPERSAND("&"),
+    PIPE("|"),
+    QUESTION("?"),
+    COLON(":"),
+    DOUBLE_AMPERSAND("&&"),
+    DOUBLE_PIPE("||"),
+
+    // Literals
+    IDENTIFIER,
+    INTEGER_LITERAL,
+    DOUBLE_LITERAL,
+    STRING_LITERAL,
+
+    // Types
+    INT("int"),
+    DOUBLE("double"),
+    BOOLEAN("boolean"),
+    FLOAT("float"),
+
+    // End of input
+    EOF;
+
+    private final String symbol;
+
+    private static final Map<String, TokenType> KEYWORDS = new HashMap<>();
+    static {
+        KEYWORDS.put("function", FUNCTION);
+        KEYWORDS.put("return", RETURN);
+        KEYWORDS.put("if", IF);
+        KEYWORDS.put("else", ELSE);
+        KEYWORDS.put("while", WHILE);
+        KEYWORDS.put("for", FOR);
+        KEYWORDS.put("var", VAR);
+        KEYWORDS.put("true", TRUE);
+        KEYWORDS.put("false", FALSE);
+        KEYWORDS.put("int", INT);
+        KEYWORDS.put("double", DOUBLE);
+        KEYWORDS.put("float", FLOAT);
+        KEYWORDS.put("boolean", BOOLEAN);
+        // Add more keywords as needed
+    }
+
+    TokenType() {
+        this.symbol = null;
+    }
+
+    TokenType(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public static TokenType getKeywordTokenType(String keyword) {
+        return KEYWORDS.getOrDefault(keyword, IDENTIFIER);
+    }
+
+    public static TokenType getSymbolTokenType(char symbol) {
+        return switch (symbol) {
+            case '(' -> LPAREN;
+            case ')' -> RPAREN;
+            case '{' -> LBRACE;
+            case '}' -> RBRACE;
+            case '[' -> LBRACKET;
+            case ']' -> RBRACKET;
+            case ';' -> SEMICOLON;
+            case ',' -> COMMA;
+            case '.' -> DOT;
+            case '+' -> PLUS;
+            case '-' -> MINUS;
+            case '*' -> ASTERISK;
+            case '/' -> SLASH;
+            case '%' -> PERCENT;
+            case '^' -> CARET;
+            case '!' -> BANG;
+            case '=' -> EQUAL;
+            case '<' -> LESS;
+            case '>' -> GREATER;
+            case '&' -> AMPERSAND;
+            case '|' -> PIPE;
+            case '?' -> QUESTION;
+            case ':' -> COLON;
+            default -> null;
+        };
+    }
+}
